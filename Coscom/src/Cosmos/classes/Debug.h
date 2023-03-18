80db81de06cd3ccb8310fe11819cbdedcf890b73
dream727 <dream77070@163.com>
2023-03-18 15:34:23 +0800

    updata

--- Coscom/src/Cosmos/classes/Debug.h
+++ Coscom/src/Cosmos/classes/Debug.h
@@ -1,7 +1,9 @@
 #ifndef DEBUG_H
 #define DEBUG_H
-#include <Cosmos/Core/Core.h>
+#include <Cosmos/core/Core.h>
+#include <string>
 namespace Cosmos {
-	CM_API void Log();
+	CM_API void Log(...);
+	CM_API void Log(std::string format, ...);
 }
 #endif // !DEBUG_H
